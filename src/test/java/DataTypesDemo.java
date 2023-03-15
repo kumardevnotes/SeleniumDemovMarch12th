27412491caba09edd113d530d63c9c6cac60b5c4
kumardevnotes <kumar.thoudishetti@gmail.com>
2023-03-15 11:30:53 +0530

    added new files for data types

--- src/test/java/DataTypesDemo.java
+++ src/test/java/DataTypesDemo.java
@@ -1,27 +1,32 @@
 
-public class DemoClass {
+public class DataTypesDemo {
 
 	public static void main(String[] abc) {
-		//data types
+		// data types
 		// EmpDetails -- empId, empName, empSalary, empGender, empMarried, empIncentive
-		int empId = 1122; //Integer datatype
+		int empId = 1122; // Integer datatype
 		String empName = "John Legened";
-		float empSalary  = 30000.560790898900f;
-		char empGender = 'M'; //character
-		boolean empMarried = true; //false indiates unmarried
+		float empSalary = 30000.560790898900f;
+		char empGender = 'M'; // character
+		boolean empMarried = true; // false indiates unmarried
 		double empIncentive = 4000.5060607879899088;
-		
+
 		System.out.println(empId);
 		System.out.println(empName);
 		System.out.println(empSalary);
 		System.out.println(empGender);
 		System.out.println(empMarried);
 		System.out.println(empIncentive);
-		
+
 		empSalary = 25000;
 		System.out.println(empSalary);
-		
-		
-		
+
+		//calling a method in main()
+		displayEmpCity();
+	}
+
+	//Declared a method
+	public static void displayEmpCity() {
+		System.out.println("Hyd");
 	}
 }
---
+++ src/test/java/com/java/methods/MethodsDemo.java
@@ -0,0 +1,48 @@
+package com.java.methods;
+
+public class MethodsDemo {
+
+	//static method
+	public static void main(String[] args) {
+		System.out.println("Hello All!");
+		
+		displayEmpName();
+		displayEmpName("Mike Miller");
+		
+		displayEmpName();
+		displayEmpName("Kumara Sangakkara");
+		
+		displayEmpSal(50000);
+		displayEmpSal(20000);
+		displayEmpSal(100000);
+		displayEmpSal(5000);
+		
+		displayEmpName("Kumar", "Thoudi");
+	}
+	
+	//method overloading in Java
+	public static void displayEmpName() {
+		System.out.println("John Legend");
+	}
+	
+	public static void displayEmpName(String fullName) { //Mike Miller
+		System.out.println(fullName); //Mike Miller
+	}
+	
+	public static void displayEmpName(String firstName, String lastName) {
+		System.out.println(firstName+lastName); 
+		System.out.println(firstName); 
+		System.out.println(lastName); 
+	}
+	
+	
+	public static void displayEmpSal(int empSal) {
+		if(empSal > 30000) { // 20000 > 30000 --> false
+			System.out.println("Emp has to pay the tax");
+		}
+		else {
+			System.out.println("Emp no need to pay the tax");
+		}
+	}
+
+}
